6a33ec6ebf9742a5accb05f3157051cd97d1278f
BethTawiah <elizabeth.tawiah@amalitech.org>
2023-12-06 11:22:16 +0000

    X AND O TURNS

--- computer.js
+++ computer.js
@@ -112,35 +112,27 @@ function restartGame() {
  updateMove();}
 
 }
+function setMsg(msg) {
+  var elem = document.getElementById("deMessage");
+  elem.innerHTML = ''; // Clear existing content
 
-document.addEventListener("DOMContentLoaded", function() {
-  // Your JavaScript code here
-  // For example, the setMsg function and the rest of your code
-  function setMsg(msg) {
-      var elem = document.getElementById("deMessage");
-      elem.innerHTML = ''; // Clear existing content
-      
-      // Create an SVG element based on the message
-      if (msg.includes("O TURN")) {
-          var userIconSVG = document.getElementById("userIconSVG");
-          if (userIconSVG) {
-              elem.innerHTML += userIconSVG.innerHTML;
-          }
-      } else if (msg.includes("X TURN")) {
-          var computerIconSVG = document.getElementById("computerIconSVG");
-          if (computerIconSVG) {
-              elem.innerHTML += computerIconSVG.innerHTML;
-          }
-      }
-
-      // Append the message text
-      elem.innerHTML += msg.replace("O TURN", "").replace("X TURN", "");
+  // Create an SVG element based on the message
+  if (msg.includes("O TURN")) {
+    // Computer is represented by "O"
+    var computerIconSVG = document.createElement("div");
+    computerIconSVG.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.7231 3.30608L16.6939 0.276913C16.3247 -0.0923043 15.7261 -0.0923043 15.3569 0.276913L10 5.63378L4.64314 0.276913C4.27392 -0.0923043 3.6753 -0.0923043 3.30608 0.276913L0.276913 3.30608C-0.0923043 3.6753 -0.0923043 4.27392 0.276913 4.64314L5.63378 10L0.276913 15.3569C-0.0923043 15.7261 -0.0923043 16.3247 0.276913 16.6939L3.30608 19.7231C3.6753 20.0923 4.27392 20.0923 4.64314 19.7231L10 14.3662L15.3569 19.7231C15.7261 20.0923 16.3247 20.0923 16.6939 19.7231L19.7231 16.6939C20.0923 16.3247 20.0923 15.7261 19.7231 15.3569L14.3662 10L19.7231 4.64314C20.0923 4.27392 20.0923 3.6753 19.7231 3.30608Z" fill="#A8BFC9"/></svg>';
+    elem.appendChild(computerIconSVG);
+  } else if (msg.includes("X TURN")) {
+    // User is represented by "X"
+    var userIconSVG = document.createElement("div");
+    userIconSVG.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="white"><path fill-rule="evenodd" clip-rule="evenodd" d="M31.7412 15.8706C31.7412 7.10551 24.6357 0 15.8706 0C7.10551 0 0 7.10551 0 15.8706C0 24.6357 7.10551 31.7412 15.8706 31.7412C24.6357 31.7412 31.7412 24.6357 31.7412 15.8706ZM9.4048 15.8706C9.4048 12.2996 12.2996 9.4048 15.8706 9.4048C19.4416 9.4048 22.3364 12.2996 22.3364 15.8706C22.3364 19.4416 19.4416 22.3364 15.8706 22.3364C12.2996 22.3364 9.4048 19.4416 9.4048 15.8706Z" fill="#A8BFC9"/></svg>';
+    elem.appendChild(userIconSVG);
   }
+  // Append the message text
+  elem.innerHTML += msg.replace("O TURN", "").replace("X TURN", "TURN");
+}
 
-  // Example usage
-  var myMove = true; // Replace with your logic to determine the move
-  setMsg(myMove ? "O TURN" : "X TURN");
-});
+
 
 
   function setAlr(msg) {
@@ -208,7 +200,7 @@ function updateMove() {
     $("#computerMessage").hide();
     $("#winMessage").hide();
   } else {
-    // $("#message").text(myMove ? setMsg(computerIconSVG+" TURN") : setMsg(userIconSVG+" TURN"));
+    $("#message").text(myMove ? setMsg("O TURN") : setMsg(" X "+"TURN"));
   }
 
   if (winner == 1 || winner == 0 || winner == -1) {
