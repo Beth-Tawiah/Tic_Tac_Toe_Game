d0257a5498e8516e704adc0dc393539f072a8afd
BethTawiah <elizabeth.tawiah@amalitech.org>
2023-12-11 18:31:58 +0000

    fixed all minor errors

--- 2player.js
+++ 2player.js
@@ -2,38 +2,40 @@ function navigateToPlayerPage(url) {
   // Handle navigation to CPU page (replace with your logic)
   window.location.href = url;
 }
+$(document).ready(function() {
 
 var player2Score = 0;
 var player1Score = 0;
 var tiesScore = 0;
 var scoresUpdated = false; 
 
-$(document).ready(function() {
+function startGame() {
+  clearAllBoxes();
+  document.turn = Math.random() < 0.5 ? "X" : "O";
+  document.winner = null;
+}
 
-  function startGame() {
-    
-     clearBox("one");
-     clearBox("two");
-     clearBox("three");
-     clearBox("four");
-     clearBox("five");
-     clearBox("six");
-     clearBox("seven");
-     clearBox("eight");
-     clearBox("nine");
-  
+function clearAllBoxes() {
+  $(".col-xs-4").html("");
+}
+
+function clearBox(number) {
+  document.getElementById(number).innerText = "";
+}
+
+function setMsg(msg, turnText) {
+  const elem = document.getElementById("deMessage");
+  elem.innerHTML = `<div>${msg.includes("O TURN") ? getOSVG() : getXSVG()}</div>${turnText}${msg.replace("O TURN", "").replace("X TURN", "")}`;
+}
+
+function getOSVG() {
+  return '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M20 10C20 4.47715 15.5228 0 10 0C4.47715 0 0 4.47715 0 10C0 15.5228 4.47715 20 10 20C15.5228 20 20 15.5228 20 10ZM5.92593 10C5.92593 7.74995 7.74995 5.92593 10 5.92593C12.25 5.92593 14.0741 7.74995 14.0741 10C14.0741 12.25 12.25 14.0741 10 14.0741C7.74995 14.0741 5.92593 12.25 5.92593 10Z" fill="#A8BFC9"/></svg>';
+}
+
+function getXSVG() {
+  return '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="15" viewBox="0 0 20 20" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M15.002 1.147 32 18.145 48.998 1.147a3 3 0 0 1 4.243 0l9.612 9.612a3 3 0 0 1 0 4.243L45.855 32l16.998 16.998a3 3 0 0 1 0 4.243l-9.612 9.612a3 3 0 0 1-4.243 0L32 45.855 15.002 62.853a3 3 0 0 1-4.243 0L1.147 53.24a3 3 0 0 1 0-4.243L18.145 32 1.147 15.002a3 3 0 0 1 0-4.243l9.612-9.612a3 3 0 0 1 4.243 0Z" fill="#31C3BD" fill-rule="evenodd"/></svg>';
+}
    
-    if (Math.random() < 0.5) {
-    document.turn = "X";} else {
-      document.turn="O";
-    }
-    document.winner = null;
-  
-
-   
-  }
-  
-
   
   $("#Restart").click(function () {
     $("#popupMessage").show(); // Show the "Restart Game?" message
@@ -84,11 +86,10 @@ $(document).ready(function() {
     
    
   });
-  function setMsg(msg) {
+  function setMsg(msg, turnText) {
     var elem = document.getElementById("deMessage");
-    elem.innerHTML = ''; // Clear existing content
-
-    // Create an SVG element based on the message
+    elem.innerHTML = ''; 
+  // svg for the message
     if (msg.includes("O TURN")) {
       var computerIconSVG = document.createElement("div");
       computerIconSVG.innerHTML='<svg xmlns="http://www.w3.org/2000/svg" width="20" height="15" viewBox="0 0 20 20" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.7231 3.30608L16.6939 0.276913C16.3247 -0.0923043 15.7261 -0.0923043 15.3569 0.276913L10 5.63378L4.64314 0.276913C4.27392 -0.0923043 3.6753 -0.0923043 3.30608 0.276913L0.276913 3.30608C-0.0923043 3.6753 -0.0923043 4.27392 0.276913 4.64314L5.63378 10L0.276913 15.3569C-0.0923043 15.7261 -0.0923043 16.3247 0.276913 16.6939L3.30608 19.7231C3.6753 20.0923 4.27392 20.0923 4.64314 19.7231L10 14.3662L15.3569 19.7231C15.7261 20.0923 16.3247 20.0923 16.6939 19.7231L19.7231 16.6939C20.0923 16.3247 20.0923 15.7261 19.7231 15.3569L14.3662 10L19.7231 4.64314C20.0923 4.27392 20.0923 3.6753 19.7231 3.30608Z" fill="#A8BFC9"/></svg>';
@@ -102,8 +103,8 @@ $(document).ready(function() {
     // Append the message text
     elem.innerHTML += msg.replace("O TURN", "").replace("X TURN", "");
   
-  }
-
+    }
+  
   
   
 
@@ -168,34 +169,26 @@ function tieMessage() {
   $("#tieMessage").show();
   $("#tieMessage p").text("ROUND TIED");
 }
-
-
-
-  
   startGame();
 
 
-  $("#start").click(function() {
-    
-    startGame();
-  })
+  // $("#start").click(function() {
 
-  
+  //   startGame();
+  // })
   $(".col-xs-4").click(function() {
-     
-
-   if (document.winner == "D") {setAlr ("Game already over. It was a draw.")}
-    
-    else
-    if (document.winner != null) {setAlr(document.turn + " already won the game");}
-    else if ($(this).html() === "") {
+    if (document.winner == "D") {
+      setAlr("Game already over. It was a draw.");
+    } else if (document.winner != null) {
+      setAlr(document.turn + " already won the game");
+    } else if ($(this).html() === "") {
       $(this).html(document.turn);
       switchTurn();
     } else {
       setWarn("That square has already been used");
     }
-  
   });
+
 
   function getBox(number) {
     return document.getElementById(number).innerText;
@@ -248,14 +241,10 @@ function tieMessage() {
     
   }
 
-
-
-
-
   
-  function clearBox(number) {
-  document.getElementById(number).innerText=""; 
-    
-  }
+  startGame();
 
+  $("#start").click(function() {
+    startGame();
+  });
 });
--- computer.js
+++ computer.js
@@ -1,5 +1,5 @@
 function navigateToCPUPage(url) {
-  // Handle navigation to CPU page (replace with your logic)
+  // Handle navigation to CPU page
   window.location.href = url;
 }
 $("#Restart").click(function () {
@@ -20,7 +20,7 @@ $("#confirmRestart").click(function () {
 
 // For computerMessage
 $("#quitComputer").click(function () {
-  // Redirect to the newGame page or perform other actions
+  // Redirect to the newGame page 
   window.location.href = 'index.html';
 });
 
@@ -30,11 +30,10 @@ $("#nextRoundComputer").click(function () {
     restartGame();
   });
   
-  // You may want to reset the game state or perform other actions
 
 // For winMessage
 $("#quitWin").click(function () {
-  // Redirect to the newGame page or perform other actions
+  // Redirect to the newGame page
   window.location.href = 'index.html';
 });
 
@@ -42,8 +41,7 @@ $("#nextRoundButton").click(function () {
   $("#winMessage").hide(); 
  
   restartGame();
-  // Continue with the game logic for the next round
-  // You may want to reset the game state or perform other actions
+  // Continue with the game  for the next round
 });
 
 
@@ -58,8 +56,10 @@ $("#nextRoundTie").click(function () {
   restartGame();
   
 });
+
+//x and o icons 
 var player="X";
-var computer="O";
+var computer="0";
 
 var playerScore = 0;
 var cpuScore = 0;
@@ -109,7 +109,7 @@ function restartGame() {
 function setMsg(msg, turnText) {
   var elem = document.getElementById("deMessage");
   elem.innerHTML = ''; 
-//create svg for the 
+// svg for the message
   if (msg.includes("O TURN")) {
     var computerIconSVG = document.createElement("div");
     computerIconSVG.innerHTML='<svg xmlns="http://www.w3.org/2000/svg" width="20" height="15" viewBox="0 0 20 20" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.7231 3.30608L16.6939 0.276913C16.3247 -0.0923043 15.7261 -0.0923043 15.3569 0.276913L10 5.63378L4.64314 0.276913C4.27392 -0.0923043 3.6753 -0.0923043 3.30608 0.276913L0.276913 3.30608C-0.0923043 3.6753 -0.0923043 4.27392 0.276913 4.64314L5.63378 10L0.276913 15.3569C-0.0923043 15.7261 -0.0923043 16.3247 0.276913 16.6939L3.30608 19.7231C3.6753 20.0923 4.27392 20.0923 4.64314 19.7231L10 14.3662L15.3569 19.7231C15.7261 20.0923 16.3247 20.0923 16.6939 19.7231L19.7231 16.6939C20.0923 16.3247 20.0923 15.7261 19.7231 15.3569L14.3662 10L19.7231 4.64314C20.0923 4.27392 20.0923 3.6753 19.7231 3.30608Z" fill="#A8BFC9"/></svg>';
@@ -123,13 +123,13 @@ function setMsg(msg, turnText) {
   // Append the message text
   elem.innerHTML += msg.replace("O TURN", "").replace("X TURN", "");
 
-}
+  }
 
 
   function setAlr(msg) {
     var elem = document.getElementById("deMessage");
     elem.innerText = msg;
-    elem.style.color = "blue";
+    elem.style.color = "#A8BFC9";
   }
 
 
@@ -148,29 +148,44 @@ function setMsg(msg, turnText) {
 
 
 $(document).ready(function() {
-  $(".col-xs-4").click(function() {
-   
-    if (winner == 1) {setAlr("Game is over, the computer won.");
-                    }
-    else if (winner == -1) {setAlr("Game is over, it was a draw.");}
-    else if (winner == 0) {setAlr("Game is over, you won!");}
-    else if ($(this).html() === "") {{
+  $(".col-xs-4").click(function () {
+    var cell = $(this).attr("id");
+    var row = parseInt(cell[1]);
+    var col = parseInt(cell[2]);
   
-    var cell = $(this).attr("id")
-    var row = parseInt(cell[1])
-    var col = parseInt(cell[2])
-    if (!myMove) {
-      board[row][col] = false;
-      myMove = true;
-      updateMove();
-      makeMove();
-    }}}
-    else {
-      setWarn("That square has already been used");
-    }
-  });
-  
-  
+    if (winner == 1) {
+      setAlr("Game is over, the computer won.");
+    } else if (winner == -1) {
+      setAlr("Game is over, it was a draw.");
+    } else if (winner == 0) {
+      setAlr("Game is over, you won!");
+    } else if (board[row][col] === null) {
+      if (!myMove) {
+        board[row][col] = false;
+        myMove = true;
+        updateMove();
+        makeMove();
+      } else {
+        // Replace the content of the clicked cell with the SVG for O
+        board[row][col] = true;
+        updateSquares();
+      }} else if ($(this).html() === "") {
+        // Dynamically insert the player's icon into the clicked cell
+        var playerIcon = document.turn === "X"
+            ? '<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M15.002 1.147 32 18.145 48.998 1.147a3 3 0 0 1 4.243 0l9.612 9.612a3 3 0 0 1 0 4.243L45.855 32l16.998 16.998a3 3 0 0 1 0 4.243l-9.612 9.612a3 3 0 0 1-4.243 0L32 45.855 15.002 62.853a3 3 0 0 1-4.243 0L1.147 53.24a3 3 0 0 1 0-4.243L18.145 32 1.147 15.002a3 3 0 0 1 0-4.243l9.612-9.612a3 3 0 0 1 4.243 0Z" fill="#31C3BD" fill-rule="evenodd"/></svg>'
+            : '<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" fill="#F2B137"/></svg>';
+
+        $(this).html(playerIcon);
+        switchTurn();
+    } else {
+        // Set the warning message to the appropriate SVG
+        var warnIcon = document.turn === "X"
+            ? '<svg width="66" height="66" xmlns="http://www.w3.org/2000/svg"><path d="M33 1c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C15.327 65 1 50.673 1 33 1 15.327 15.327 1 33 1Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" stroke="#F2B137" stroke-width="2" fill="none"/></svg>'
+            : '<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M51.12 1.269c.511 0 1.023.195 1.414.586l9.611 9.611c.391.391.586.903.586 1.415s-.195 1.023-.586 1.414L44.441 32l17.704 17.705c.391.39.586.902.586 1.414 0 .512-.195 1.024-.586 1.415l-9.611 9.611c-.391.391-.903.586-1.415.586a1.994 1.994 0 0 1-1.414-.586L32 44.441 14.295 62.145c-.39.391-.902.586-1.414.586a1.994 1.994 0 0 1-1.415-.586l-9.611-9.611a1.994 1.994 0 0 1-.586-1.415c0-.512.195-1.023.586-1.414L19.559 32 1.855 14.295a1.994 1.994 0 0 1-.586-1.414c0-.512.195-1.024.586-1.415l9.611-9.611c.391-.391.903-.586 1.415-.586s1.023.195 1.414.586L32 19.559 49.705 1.855a1.994 1.994 0 0 1 .586-1.415c0-.512.195-1.024.586-1.414Z" stroke="#31C3BD" stroke-width="2" fill="none"/></svg>';
+
+        $(this).html(warnIcon);
+      }
+    });
 
 });
 
@@ -269,10 +284,23 @@ function getWinner(board) {
 function updateSquares() {
   for (var i = 0; i < 3; i++) {
     for (var j = 0; j < 3; j++) {
-      $("#c" + i + "" + j).text(board[i][j] == false ? player : board[i][j] == true ? computer : "");
+      var cellContent = "";
+      if (board[i][j] === false) {
+        // Player's move (X)
+        cellContent ='<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M15.002 1.147 32 18.145 48.998 1.147a3 3 0 0 1 4.243 0l9.612 9.612a3 3 0 0 1 0 4.243L45.855 32l16.998 16.998a3 3 0 0 1 0 4.243l-9.612 9.612a3 3 0 0 1-4.243 0L32 45.855 15.002 62.853a3 3 0 0 1-4.243 0L1.147 53.24a3 3 0 0 1 0-4.243L18.145 32 1.147 15.002a3 3 0 0 1 0-4.243l9.612-9.612a3 3 0 0 1 4.243 0Z" fill="#31C3BD" fill-rule="evenodd"/></svg>'
+
+      } else if (board[i][j] === true) {
+        // Computer's move (O)
+        cellContent ='<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" fill="#F2B137"/></svg>'
+
+      }
+
+      // Set the content of the cell
+      $("#c" + i + "" + j).html(cellContent);
     }
   }
 }
+
 
 function makeMove() {
   setTimeout(function () {
