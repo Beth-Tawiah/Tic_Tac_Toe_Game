1ef47e076e427a74a10fccc6bd59ac1ab50dbe55
BethTawiah <elizabeth.tawiah@amalitech.org>
2023-12-18 18:28:55 +0000

    2 player all hover done

--- computer.js
+++ computer.js
@@ -12,7 +12,7 @@ $("#cancel").click(function () {
 
 $("#confirmRestart").click(function () {
   $("#popupMessage").hide(); 
-  restartGame();
+  startGame();
 });
 
 
@@ -28,7 +28,7 @@ $("#nextRoundComputer").click(function () {
   $(".winning-cell-x").removeClass("winning-cell-x");
   $(".winning-cell-o").removeClass("winning-cell-o");
 
-  restartGame();
+  startGame();
 });
 
   
@@ -42,7 +42,7 @@ $("#quitWin").click(function () {
 $("#nextRoundButton").click(function () {
   $("#winMessage").hide(); 
  
-  restartGame();
+  startGame();
   // Continue with the game  for the next round
 });
 
@@ -55,16 +55,13 @@ $("#quitTie").click(function () {
 $("#nextRoundTie").click(function () {
   $("#tieMessage").hide(); 
  
-  restartGame();
+  startGame();
   
 });
 
 //x and o icons 
 var player="X";
 var computer="0";
-// var xIcon = '<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M15.002 1.147 32 18.145 48.998 1.147a3 3 0 0 1 4.243 0l9.612 9.612a3 3 0 0 1 0 4.243L45.855 32l16.998 16.998a3 3 0 0 1 0 4.243l-9.612 9.612a3 3 0 0 1-4.243 0L32 45.855 15.002 62.853a3 3 0 0 1-4.243 0L1.147 53.24a3 3 0 0 1 0-4.243L18.145 32 1.147 15.002a3 3 0 0 1 0-4.243l9.612-9.612a3 3 0 0 1 4.243 0Z" fill="#31C3BD" fill-rule="evenodd"/></svg>';
-// var oIcon = '<svg width="66" height="66" xmlns="http://www.w3.org/2000/svg"><path d="M33 1c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C15.327 65 1 50.673 1 33 1 15.327 15.327 1 33 1Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" stroke="#F2B137" stroke-width="2" fill="none"/></svg>';
-
 var playerScore = 0;
 var cpuScore = 0;
 var tiesScore = 0;
@@ -92,7 +89,7 @@ if (myMove) {
   makeMove();
 }
 
-function restartGame() {
+function startGame() {
   board = [
     [null, null, null],
     [null, null, null],
